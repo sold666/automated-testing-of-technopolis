abb3b40f2b3749b183ffe306bbdf83e6388e2c31
sold666 <korch937@mail.ru>
2023-03-28 00:59:06 +0300

    add new tests, use PageObject pattern

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,11 +8,25 @@ public class LoginPage {
     private final SelenideElement emailField = $("#field_email");
     private final SelenideElement passwordField = $("#field_password");
     private final SelenideElement loginButton = $("input[class='button-pro __wide']");
+    private final SelenideElement loginForm = $("form[name='frmLogin']");
+    private static final SelenideElement loginError = $("div[class='input-e login_error']");
 
     public MainPage login(String email, String password) {
         emailField.setValue(email);
         passwordField.setValue(password);
         loginButton.click();
         return new MainPage();
+    }
+
+    public SelenideElement getLoginForm() {
+        return loginForm;
+    }
+
+    public SelenideElement getLoginButton() {
+        return loginButton;
+    }
+
+    public SelenideElement getLoginError() {
+        return loginError;
     }
 }
